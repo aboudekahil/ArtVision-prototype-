66eb88b448c6ae5160a54977b60f873eeed3ee46
aboude kahil <aboudeh003@gmail.com>
2023-10-27 22:58:54 +0300

    homepage header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,10 +1,20 @@
+import { Link } from "react-router-dom";
+
 const Header: React.FC = () => {
   return (
     <header className="bg-ArtBlack2 w-full h-20">
       <nav className="w-full flex items-center align-middle h-full">
-        <figure>
-          <img className="w-8 m-8" src="/Logo.svg" />
-        </figure>
+        <Link to={"/"}>
+          <figure className="m-5 hover:motion-safe:animate-[ping_1s_ease-in-out]">
+            <img className="w-8" src="/Logo.svg" />
+          </figure>
+        </Link>
+
+        <Link to={"/following"}>
+          <h1 className="capitalize text-xl hover:text-Porple font-semibold m-5">
+            Following
+          </h1>
+        </Link>
       </nav>
     </header>
   );
--- frontend/src/pages/BasePage.tsx
+++ frontend/src/pages/BasePage.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import LocalStorageHandler from "../utils/LocalStorageHandler";
 import { LocalStorageUser } from "../shared.types";
@@ -6,10 +6,18 @@ import Header from "../components/Header";
 
 const BasePage = () => {
   const navigate = useNavigate();
+
+  const [artUser, setArtUser] = useState<LocalStorageUser>();
+
   useEffect(() => {
     let user = LocalStorageHandler.getFromKeyParsed<LocalStorageUser>("curru");
 
-    if (!user) navigate("/signin");
+    if (!user) {
+      navigate("/signin");
+      return;
+    }
+
+    setArtUser(user);
   }, []);
 
   return (
